12256e19c8194d1a2f8b0f05bcaf75824702596d
leonardo <leonardo.pinheiro.silva30@escola.pr.gov.br>
2025-05-29 13:16:14 -0300

    Add files via upload

---
+++ fhashcard-main/flashcard2/perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'fla',
+    'Qual é a data de fundação do Flamengo?',
+    'O Flamengo foi fundado em 17 de novembro de 1895.'
+)
+
+criaCartao(
+    'fla',
+    'Qual é o estádio onde o Flamengo manda seus jogos?',
+    'O Flamengo manda seus jogos no Estádio do Maracanã.'
+)
+
+criaCartao(
+    'fla',
+    'Quantos títulos do Campeonato Brasileiro o Flamengo possui?',
+    'O Flamengo possui 38 títulos do Campeonato Brasileiro.'
+)
+
+criaCartao(
+    'fla',
+    'Qual é o mascote do Flamengo?',
+    'O mascote do Flamengo é um urubu.'
+)
+
+
+
+criaCartao(
+    'fla',
+    'Qual é a principal rivalidade do Flamengo?',
+    'A principal rivalidade  é com o Fluminense.'
+)
+
+
+
+criaCartao(
+    'fla',
+    'Quem é considerado um dos maiores ídolos da história do Flamengo?',
+    ' Zico é considerado um dos maiores ídolos da história do Flamengo.'
+)
